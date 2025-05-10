f593cd5767659b8b18d56370660f0871460ef1d1
Santhosh <sbuvanes@utk.edu>
2025-05-10 13:27:04 +0530

    CORS updated

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const corsOptions = {
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-key'], // <-- add this line
 };
 
