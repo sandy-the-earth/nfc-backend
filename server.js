11c18b3bdc3bf8b711c18fca4a784ffe1bf4ac8d
Santhosh <sbuvanes@utk.edu>
2025-05-10 12:18:47 +0530

    updated

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const corsOptions = {
   },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-key'], // <-- add this line
 };
 
 // ✅ Apply CORS to all requests including preflight
