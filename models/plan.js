9f5b8f95a39dedcad006a6d25957d19a30cd0f3b
Santhosh <sbuvanes@utk.edu>
2025-06-16 13:16:57 +0530

    Refactor plan features for improved clarity and consistency

--- models/plan.js
+++ models/plan.js
@@ -14,19 +14,30 @@ const plans = [
     id: 'novice',
     name: 'Novice',
     prices: { monthly: 99, quarterly: 199 },
-    features: ['Basic Profile Detailing', 'Exchange Contact - 10 credits/month', 'Basic Insights- Profile views, Unique visitors', 'Card Insurance*-Upto 2 months' ]
+    features: ['Basic Profile Detailing', 
+        'Contact Exchanges - 10 credits/month', 'Basic Insights- Profile views, Unique visitors Count', 
+        'Profile Customization - Classic & Chrome - 3 colors', 'Card Insurance*-Upto 2 months' ]
   },
   {
     id: 'corporate',
     name: 'Corporate',
     prices: { monthly: 199, quarterly: 399 },
-    features: ['Advanced Profiling', 'Company details, Brochures', 'Calendar Blocking', 'Exchange Contact - 50 credits/month', 'Industry wise insights, intermediate profile analytics', 'Card Insurance*-Upto 6 months']
+    features: ['Advanced Profiling', 'Company details, Brochures', 
+        'Calendar Blocking', 'Contact Exchanges - 50 credits/month', 
+        'Industry wise insights, intermediate profile analytics',
+        'Profile Customization - Classic & Chrome - 3 colors', 
+        'Card Insurance*-Upto 6 months']
   },
   {
     id: 'elite',
     name: 'Elite',
     prices: { monthly: 299, quarterly: 599 },
-    features: ['Access to all the available features', 'Get invited to closed invite-only networking events', 'Detailed profile performance analysis', 'Founder-level insights', 'Unlimited contact exchanges', 'Exclusive Badge on Profile','Card Insurance*-Upto 12 months', 'Priority Support 24x7']
+    features: ['Access to all the available features', 
+        'Get invited to closed invite-only networking events', 
+        'Detailed profile performance analysis', 'On profile calendar booking', 
+        'Unlimited contact exchanges', 'Exclusive Badge on Profile',
+        'Custom materials and colors Customization',
+        'Card Insurance*-Upto 12 months', 'Priority Support 24x7']
   },
 ];
 
